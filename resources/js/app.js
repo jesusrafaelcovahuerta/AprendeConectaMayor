009c890881966bcbdd026ffe80617f4c4de1e6de
jisjesuscova <jesuscova@jisparking.com>
2023-09-10 19:40:01 -0300

    cambios

--- resources/js/app.js
+++ resources/js/app.js
@@ -61,7 +61,7 @@ Vue.use(VueRouter);
 Vue.use(VueMask);
 Vue.prototype.$app = Plugins.App; // Agregamos el plugin App a Vue
 
-const gtagId = process.env.VUE_APP_GTAG_ID;
+const gtagId = "G-X9C2GX3926";
 
 Vue.use(VueGtag, {
     config: { id: gtagId }
